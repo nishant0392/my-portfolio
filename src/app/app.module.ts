ed9ce9dfc9715c17128a71ee0e441fb03de9cbaa
nishant kumar <nishkr0392@gmail.com>
2021-10-04 06:19:31 +0530

    Template added in angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ResumeComponent,
+    ContactComponent,
+    PortfolioComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
