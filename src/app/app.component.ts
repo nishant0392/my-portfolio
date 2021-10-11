2aedb67cf07a5309eca394597d4eb7d7777042e0
nishant kumar <nishkr0392@gmail.com>
2021-10-11 12:48:42 +0530

    Fixed typed.js not working issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myPortfolio';
+
+  public options = {
+    strings: ["Writer", "Developer"],
+    typeSpeed: 200,
+    loop: true
+  };
+
+  public typed: Typed;
+
+  ngOnInit() {
+    this.typed = new Typed('.typed-skills', this.options);
+  }
+
 }
