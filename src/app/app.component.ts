11d757e1e174199d3b3277a138299178c3b0ce7a
nishant kumar <nishkr0392@gmail.com>
2022-03-03 13:58:11 +0530

    Added social profile links.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,14 @@ export class AppComponent {
 
   public typed: Typed;
 
+  // Social Network Links
+  public socialNetworkLinks = {
+    linkedIn: "https://linkedin.com/in/nishant-kumar-793638108",
+    twitter: "https://twitter.com/Nishant0891?t=KP4fLrnsOPtkg9gFZ3V2aA&s=08",
+    facebook: "https://www.facebook.com/profile.php?id=100004960841802",
+    instagram: "https://www.instagram.com/nishkr0392/"
+  };
+
   ngOnInit() {
     this.typed = new Typed('.typed-skills', this.options);
   }
